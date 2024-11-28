06e86dfbc45a217dcb905a6f165bb4b9610a6c98
YOUR NAME <YOUR@EMAIL.com>
2024-11-29 02:42:48 +0700

    create page and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes , Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import About from "./pages/about";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>;
+  return(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="Sign-in" element={<SignIn/>} />
+    <Route path="/Sign-up" element={<SignUp/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/profile" element={<Profile/>} />
+
+    </Routes>
+    </BrowserRouter>
+  )
 }
